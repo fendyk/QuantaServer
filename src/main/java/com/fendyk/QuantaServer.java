7c7eaa49c3e99c5fbc6df17dfd67d4c513c718b6
Raul Fernandez van Dijk <raul.fernandezvandijk@gmail.com>
2023-01-30 08:30:03 +0100

    initialize

---
+++ src/main/java/com/fendyk/QuantaServer.java
@@ -0,0 +1,51 @@
+package com.fendyk;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+public class QuantaServer extends JavaPlugin implements PluginMessageListener {
+
+    @Override
+    public void onEnable() {
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", this);
+    }
+
+    @Override
+    public void onDisable() {
+        //make sure to unregister the registered channels in case of a reload
+        this.getServer().getMessenger().unregisterOutgoingPluginChannel(this);
+        this.getServer().getMessenger().unregisterIncomingPluginChannel(this);
+    }
+
+    @Override
+    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+        System.out.println("Something happeneed!S");
+        if (!channel.equals("BungeeCord")) {
+            return;
+        }
+        ByteArrayDataInput in = ByteStreams.newDataInput(message);
+        String subchannel = in.readUTF();
+        if (subchannel.equals("Subchannel")) {
+            // Use the code sample in the 'Response' sections below to read
+            // the data.
+
+            String second = in.readUTF();
+            System.out.println(second);
+        }
+        System.out.println(subchannel);
+        System.out.println(subchannel);
+
+    }
+}
